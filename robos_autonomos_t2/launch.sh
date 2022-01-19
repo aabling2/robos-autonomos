c94011739235dfe7ccb6d96bb671358835924321
aabling2 <augustoabling18@gmail.com>
2022-01-19 08:47:41 -0300

    Scripts test

---
+++ robos_autonomos_t2/launch.sh
@@ -0,0 +1,7 @@
+# usr/bin/bash!
+#export $ROS_PACKAGE_PATH=$PWD
+rospack profile
+# roslaunch jackal_gazebo jackal_world.launch
+# roslaunch jackal_gazebo t2.launch config:=front_laser
+# roslaunch jackal_viz view_robot.launch
+roslaunch robos_autonomos_t2 t2.launch
