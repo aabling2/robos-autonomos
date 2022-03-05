f03dc6f2114bf253986b12eb0556458c4e27e34a
atman-aabling <augusto.abling@atmansystems.com>
2022-03-05 18:34:02 -0300

    Refac.

--- trabalho3/install/install_ros_packages.sh
+++ trabalho3/install/install_ros_packages.sh
@@ -9,4 +9,4 @@ sudo apt-get install ros-melodic-jackal-simulator ros-melodic-jackal-desktop ros
 # Path generator
 ROOT_DIR=$(dirname $(dirname $(realpath ${BASH_SOURCE})))
 sudo git clone https://github.com/ricardocmello/path_generator.git $ROOT_DIR/src/path_generator/
-sudo chown -Rc $USER $ROOT_DIR/src/path_generator
+sudo chown -Rc $USER:$USER $ROOT_DIR/src/path_generator
